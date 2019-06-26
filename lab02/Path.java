5cc366ab1c431368474c2b7538f75f16fa88989f
“AbigailCY” <rollingcy@icloud.com>
2019-06-26 09:02:45 -0700

    5th Path

--- lab02/Path.java
+++ lab02/Path.java
@@ -7,7 +7,9 @@ public class Path {
 
 
     public Path (double x, double y){
-        Point Curr = new Point(x, y);
+        this.Next = new Point(x, y);
+        // this.Next.setX(x);
+        // this.Next.setY(y);
     }
 
     public double getCurrX() {
@@ -33,15 +35,18 @@ public class Path {
     }
 
     public void iterate(double dx, double dy){
-        this.Curr = this.Next;
+        this.Curr.setX(this.Next.getX()); 
+        this.Curr.setY(this.Next.getY());
         this.Next.setX(this.Curr.getX()+dx);
         this.Next.setY(this.Curr.getY()+dy);
     }
 
     public static void main(String[] args){
-        Path mypa = new Path(1, 2);
+        Path mypa = new Path(0, 1);
         Point pa = new Point(3,4);
         mypa.setCurrentPoint(pa);
-        // System.out.println( mypa.getCurrX()+mypa.getCurrY());
+        mypa.iterate(5, 5);
+        System.out.println(mypa.getCurrX()+" "+ mypa.getCurrY()+ " "+mypa.getNextX()+" "+mypa.getNextY());
+        // System.out.println(String.format("%.1f %.1f", mypa.getNextX(), mypa.getNextY()));
     }
 }
