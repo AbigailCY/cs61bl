a14a760fbc5fa0263e6d829e83afba3fd63c6005
“AbigailCY” <rollingcy@icloud.com>
2019-06-26 08:47:07 -0700

    Path 4th

--- lab02/Path.java
+++ lab02/Path.java
@@ -1,3 +1,5 @@
+
+
 /** A class that represents a path via pursuit curves. */
 public class Path {
     public Point Curr;
@@ -5,8 +7,7 @@ public class Path {
 
 
     public Path (double x, double y){
-        this.Next.setX(x);
-        this.Next.setY(y);
+        Point Curr = new Point(x, y);
     }
 
     public double getCurrX() {
@@ -33,17 +34,14 @@ public class Path {
 
     public void iterate(double dx, double dy){
         this.Curr = this.Next;
-
         this.Next.setX(this.Curr.getX()+dx);
         this.Next.setY(this.Curr.getY()+dy);
     }
 
     public static void main(String[] args){
         Path mypa = new Path(1, 2);
-        // mypa.Curr.setX(4);
-        // mypa.Curr.setX(5);
-        // System.out.println(mypa.getCurrX(), mypa.getCurrY(), mypa.getNextX(), mypa.getNextY(), mypa.getCurrentPoint().getX());
-        // System.out.println("m");
-
+        Point pa = new Point(3,4);
+        mypa.setCurrentPoint(pa);
+        // System.out.println( mypa.getCurrX()+mypa.getCurrY());
     }
 }
