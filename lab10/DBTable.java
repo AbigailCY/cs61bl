7a932665e8dba37ec51f5941fd0b8ee0ee6f1133
AbigailCY <rollingcy@icloud.com>
2019-07-13 00:57:05 -0700

    style checked

--- lab10/DBTable.java
+++ lab10/DBTable.java
@@ -1,4 +1,8 @@
-import java.util.*;
+import java.util.List;
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.Objects;
+import java.util.Arrays;
 import java.util.function.Function;
 
 public class DBTable<T> {
