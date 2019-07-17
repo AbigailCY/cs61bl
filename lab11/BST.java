8544f530741b1cda8825cca7ecb17523810f6fcb
AbigailCY <rollingcy@icloud.com>
2019-07-17 03:53:04 -0700

    question 1.1

--- lab11/BST.java
+++ lab11/BST.java
@@ -15,24 +15,26 @@ public class BST<T> {
     private BSTNode<T> sortedQueueToTree(Queue<T> Q, int N) {
         // HINT: Remember to pass the same Queue to recursive calls, to
         //			take advantage of the fact that it is sorted.
-        T[] arrTree = (T[])new Object[N];
+        if (N == 0) {
+            return root;
+        }
+        T[] arrTree = (T[]) new Object[N];
         for (int i = 0; i < N; i++) {
             T temp = Q.poll();
             arrTree[i] = temp;
         }
-        root = sqtHelper(arrTree, 0, N);
+        root = sqtHelper(arrTree, 0, N-1)
         return root;
     }
 
     private BSTNode<T> sqtHelper(T[] arrTree,int start, int end) {
-        BSTNode<T> temp;
         if (start > end) {
             return null;
         }
         int mid = (start + end) / 2;
-        temp = new BSTNode<> (arrTree[mid]);
+        BSTNode<T> temp = new BSTNode<> (arrTree[mid]);
         temp.left = sqtHelper(arrTree, start, mid-1);
-        temp.right = sqtHelper(arrTree, mid, end);
+        temp.right = sqtHelper(arrTree, mid+1, end);
 
         return temp;
     }
