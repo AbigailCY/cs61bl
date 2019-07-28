7483421fa8b395d417197f7e4da064253cc9c378
AbigailCY <rollingcy@icloud.com>
2019-07-27 18:52:33 -0700

    merge modified 16

--- proj2/gitlet/Gitlet.java
+++ proj2/gitlet/Gitlet.java
@@ -344,9 +344,9 @@ public class Gitlet implements Serializable {
 //        HashSet<String> toChange = new HashSet<>();
         for (String file : splitCon.keySet()) {
             if (currCon.containsKey(file) && givenCon.containsKey(file)) {
+//                && !currCon.get(file).equals(givenCon.get(file))
                 if (!currCon.get(file).equals(splitCon.get(file))
-                        && !givenCon.get(file).equals(splitCon.get(file))
-                        && !currCon.get(file).equals(givenCon.get(file))) {
+                        && !givenCon.get(file).equals(splitCon.get(file))) {
                     toMerge1.add(file);
                 } else if (currCon.get(file).equals(splitCon.get(file))
                         && !givenCon.get(file).equals(splitCon.get(file))) {
