6b7c9da5fcf6871ab35120716f66bbd14f474c2e
AbigailCY <rollingcy@icloud.com>
2019-07-27 17:22:05 -0700

    merge modified 5

--- proj2/gitlet/Main.java
+++ proj2/gitlet/Main.java
@@ -6,35 +6,182 @@ import java.io.IOException;
 /* Driver class for Gitlet, the tiny stupid version-control system.
    @author
 */
+
 public class Main {
     /* Usage: java gitlet.Main ARGS, where ARGS contains
        <COMMAND> <OPERAND> .... */
+    public static void log_block(String[] args) throws IOException {
+        File gitletFile = new File("./.gitlet/");
+        if (!gitletFile.exists()) {
+            System.out.println("Not in an initialized gitlet directory.");
+            return;
+        }
+
+        Gitlet myGit = Gitlet.deserialize();
+        myGit.log();
+        Gitlet.serialize(myGit);
+    }
+
+    public static void status_block(String[] args) throws IOException {
+        File gitletFile = new File("./.gitlet//");
+        if (!gitletFile.exists()) {
+            System.out.println("Not in an initialized gitlet directory.");
+            return;
+        }
+
+        Gitlet myGit = Gitlet.deserialize();
+        myGit.status();
+        Gitlet.serialize(myGit);
+    }
+
+    public static void commit_block(String[] args) throws IOException {
+        File gitletFile = new File("./.gitlet/");
+        if (!gitletFile.exists()) {
+            System.out.println("Not in an initialized gitlet directory.");
+            return;
+        }
+        if (args.length == 1 || args[1].equals("")) {
+            System.out.println("Please enter a commit message.");
+            return;
+        }
+
+        String message = args[1];
+        Gitlet myGit = Gitlet.deserialize();
+        myGit.commit(message);
+        Gitlet.serialize(myGit);
+
+    }
+
+    public static void rm_block(String[] args) throws IOException {
+        File gitletFile = new File("./.gitlet/");
+        if (!gitletFile.exists()) {
+            System.out.println("Not in an initialized gitlet directory.");
+            return;
+        }
+        if (args.length == 1 || args[1].equals("")) {
+            System.out.println("Incorrect operands.");
+            return;
+        }
+
+        String message = args[1];
+        Gitlet myGit = Gitlet.deserialize();
+        myGit.rm(message);
+        Gitlet.serialize(myGit);
+    }
+
+    public static void find_block(String[] args) throws IOException {
+        File gitletFile = new File("./.gitlet/");
+        if (!gitletFile.exists()) {
+            System.out.println("Not in an initialized gitlet directory.");
+            return;
+        }
+        if (args.length == 1 || args[1].equals("")) {
+            System.out.println("Incorrect operands.");
+            return;
+        }
+
+        String message = args[1];
+        Gitlet myGit = Gitlet.deserialize();
+        myGit.find(message);
+        Gitlet.serialize(myGit);
+    }
+
+    public static void checkout_block(String[] args) throws IOException {
+        File gitletFile = new File("./.gitlet/");
+        if (!gitletFile.exists()) {
+            System.out.println("Not in an initialized gitlet directory.");
+            return;
+        }
+        if (args.length == 1 || args[1].equals("")) {
+            System.out.println("Incorrect operands.");
+            return;
+        }
+
+
+        if (args[1].equals("--") && args.length == 3) {
+            String finalArgs = args[2];
+            Gitlet myGit = Gitlet.deserialize();
+            myGit.checkout(finalArgs, 0);
+            Gitlet.serialize(myGit);
+
+        } else if (args.length == 4 && args[2].equals("--")) {
+            Gitlet myGit = Gitlet.deserialize();
+            myGit.checkout(args[1], args[3]);
+            Gitlet.serialize(myGit);
+        } else if (args.length == 2 && !(args[1].equals("--"))) {
+            Gitlet myGit = Gitlet.deserialize();
+            myGit.checkout(args[1]);
+            Gitlet.serialize(myGit);
+        } else {
+            System.out.println("Incorrect operands.");
+            return;
+        }
+    }
+
+    public static void reset_block(String[] args) throws IOException {
+        File gitletFile = new File("./.gitlet/");
+        if (!gitletFile.exists()) {
+            System.out.println("Not in an initialized gitlet directory.");
+            return;
+        }
+        if (args.length == 1 || args[1].equals("")) {
+            System.out.println("Incorrect operands.");
+            return;
+        }
+
+        String message = args[1];
+        Gitlet myGit = Gitlet.deserialize();
+        myGit.reset(message);
+        Gitlet.serialize(myGit);
+    }
+
+    public static void branch_block(String[] args) throws IOException {
+        File gitletFile = new File("./.gitlet/");
+        if (!gitletFile.exists()) {
+            System.out.println("Not in an initialized gitlet directory.");
+            return;
+        }
+        if (args.length == 1 || args[1].equals("")) {
+            System.out.println("Incorrect operands.");
+            return;
+        }
+
+        String message = args[1];
+        Gitlet myGit = Gitlet.deserialize();
+        myGit.branch(message);
+        Gitlet.serialize(myGit);
+    }
+
+    public static void merge_block(String[] args) throws IOException {
+        File gitletFile = new File("./.gitlet/");
+        if (!gitletFile.exists()) {
+            System.out.println("Not in an initialized gitlet directory.");
+            return;
+        }
+        if (args.length == 1 || args[1].equals("")) {
+            System.out.println("Incorrect operands.");
+            return;
+        }
+
+        String message = args[1];
+        Gitlet myGit = Gitlet.deserialize();
+        myGit.merge(message);
+        Gitlet.serialize(myGit);
+    }
+
     public static void main(String[] args) throws IOException {
-
 //        args = args.strip();
-
         if (args.length < 1) {
             System.out.println("Please enter a command.");
         }
-
         if (args[0].equals("init")) {
             Gitlet myGit = new Gitlet();
             myGit.init();
             Gitlet.serialize(myGit);
             return;
         }
-
         if (args[0].equals("log")) {
-            File gitletFile = new File("./.gitlet/");
-            if (!gitletFile.exists()) {
-                System.out.println("Not in an initialized gitlet directory.");
-                return;
-            }
-
-            Gitlet myGit = Gitlet.deserialize();
-            myGit.log();
-            Gitlet.serialize(myGit);
-
+            log_block(args);
         } else if (args[0].equals("global-log")) {
             File gitletFile = new File("./.gitlet/");
             if (!gitletFile.exists()) {
@@ -47,16 +194,7 @@ public class Main {
             Gitlet.serialize(myGit);
 
         } else if (args[0].equals("status")) {
-            File gitletFile = new File("./.gitlet//");
-            if (!gitletFile.exists()) {
-                System.out.println("Not in an initialized gitlet directory.");
-                return;
-            }
-
-            Gitlet myGit = Gitlet.deserialize();
-            myGit.status();
-            Gitlet.serialize(myGit);
-
+            status_block(args);
         } else if (args[0].equals("add")) {
             File gitletFile = new File("./.gitlet/");
             if (!gitletFile.exists()) {
@@ -67,108 +205,20 @@ public class Main {
                 System.out.println("Incorrect operands.");
                 return;
             }
-
             String message = args[1];
             Gitlet myGit = Gitlet.deserialize();
             myGit.add(message);
             Gitlet.serialize(myGit);
-
         } else if (args[0].equals("commit")) {
-            File gitletFile = new File("./.gitlet/");
-            if (!gitletFile.exists()) {
-                System.out.println("Not in an initialized gitlet directory.");
-                return;
-            }
-            if (args.length == 1 || args[1].equals("")) {
-                System.out.println("Please enter a commit message.");
-                return;
-            }
-
-            String message = args[1];
-            Gitlet myGit = Gitlet.deserialize();
-            myGit.commit(message);
-            Gitlet.serialize(myGit);
-
+            commit_block(args);
         } else if (args[0].equals("rm")) {
-            File gitletFile = new File("./.gitlet/");
-            if (!gitletFile.exists()) {
-                System.out.println("Not in an initialized gitlet directory.");
-                return;
-            }
-            if (args.length == 1 || args[1].equals("")) {
-                System.out.println("Incorrect operands.");
-                return;
-            }
-
-            String message = args[1];
-            Gitlet myGit = Gitlet.deserialize();
-            myGit.rm(message);
-            Gitlet.serialize(myGit);
-
+            rm_block(args);
         } else if (args[0].equals("find")) {
-            File gitletFile = new File("./.gitlet/");
-            if (!gitletFile.exists()) {
-                System.out.println("Not in an initialized gitlet directory.");
-                return;
-            }
-            if (args.length == 1 || args[1].equals("")) {
-                System.out.println("Incorrect operands.");
-                return;
-            }
-
-            String message = args[1];
-            Gitlet myGit = Gitlet.deserialize();
-            myGit.find(message);
-            Gitlet.serialize(myGit);
-
+            find_block(args);
         } else if (args[0].equals("checkout")) {
-            File gitletFile = new File("./.gitlet/");
-            if (!gitletFile.exists()) {
-                System.out.println("Not in an initialized gitlet directory.");
-                return;
-            }
-            if (args.length == 1 || args[1].equals("")) {
-                System.out.println("Incorrect operands.");
-                return;
-            }
-
-
-            if (args[1].equals("--") && args.length == 3) {
-                String finalArgs = args[2];
-                Gitlet myGit = Gitlet.deserialize();
-                myGit.checkout(finalArgs, 0);
-                Gitlet.serialize(myGit);
-
-            } else if (args.length == 4 && args[2].equals("--")) {
-                Gitlet myGit = Gitlet.deserialize();
-                myGit.checkout(args[1], args[3]);
-                Gitlet.serialize(myGit);
-            } else if (args.length == 2 && !(args[1].equals("--"))) {
-                Gitlet myGit = Gitlet.deserialize();
-                myGit.checkout(args[1]);
-                Gitlet.serialize(myGit);
-            } else {
-                System.out.println("Incorrect operands.");
-                return;
-            }
-
-
+            checkout_block(args);
         } else if (args[0].equals("branch")) {
-            File gitletFile = new File("./.gitlet/");
-            if (!gitletFile.exists()) {
-                System.out.println("Not in an initialized gitlet directory.");
-                return;
-            }
-            if (args.length == 1 || args[1].equals("")) {
-                System.out.println("Incorrect operands.");
-                return;
-            }
-
-            String message = args[1];
-            Gitlet myGit = Gitlet.deserialize();
-            myGit.branch(message);
-            Gitlet.serialize(myGit);
-
+            branch_block(args);
         } else if (args[0].equals("rm-branch")) {
             File gitletFile = new File("./.gitlet/");
             if (!gitletFile.exists()) {
@@ -184,39 +234,10 @@ public class Main {
             Gitlet myGit = Gitlet.deserialize();
             myGit.rmBranch(message);
             Gitlet.serialize(myGit);
-
         } else if (args[0].equals("reset")) {
-            File gitletFile = new File("./.gitlet/");
-            if (!gitletFile.exists()) {
-                System.out.println("Not in an initialized gitlet directory.");
-                return;
-            }
-            if (args.length == 1 || args[1].equals("")) {
-                System.out.println("Incorrect operands.");
-                return;
-            }
-
-            String message = args[1];
-            Gitlet myGit = Gitlet.deserialize();
-            myGit.reset(message);
-            Gitlet.serialize(myGit);
-
+            reset_block(args);
         } else if (args[0].equals("merge")) {
-            File gitletFile = new File("./.gitlet/");
-            if (!gitletFile.exists()) {
-                System.out.println("Not in an initialized gitlet directory.");
-                return;
-            }
-            if (args.length == 1 || args[1].equals("")) {
-                System.out.println("Incorrect operands.");
-                return;
-            }
-
-            String message = args[1];
-            Gitlet myGit = Gitlet.deserialize();
-            myGit.merge(message);
-            Gitlet.serialize(myGit);
-
+            merge_block(args);
         } else {
             System.out.println("No command with that name exists.");
         }
