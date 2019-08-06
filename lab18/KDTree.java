a1f32377b3ae4b88a948f54118c8cf88ddbe08fe
AbigailCY <rollingcy@icloud.com>
2019-08-06 03:19:35 -0700

    KDTree

--- lab18/KDTree.java
+++ lab18/KDTree.java
@@ -2,32 +2,122 @@ import java.util.List;
 
 public class KDTree implements PointSet {
 
-    // TODO: Instance variables here!
+    KDTreeNode root;
+    int size;
 
     /* Constructs a KDTree using POINTS. You can assume POINTS contains at least one
        Point object. */
     public KDTree(List<Point> points) {
-        // TODO: YOUR CODE HERE
+        this.size = points.size();
+        for (Point p : points) {
+            this.root = insert(this.root, p, true);
+        }
     }
 
-    /*
 
-    You might find this insert helper method useful when constructing your KDTree!
-    Think of what arguments you might want insert to take in. If you need
-    inspiration, take a look at how we do BST insertion!
+//    You might find this insert helper method useful when constructing your KDTree!
+//    Think of what arguments you might want insert to take in. If you need
+//    inspiration, take a look at how we do BST insertion!
 
-    private KDTreeNode insert(...) {
-        ...
+    private KDTreeNode insert(KDTreeNode node, Point key, boolean xy) {
+        if (node == null) {
+            return new KDTreeNode(key);
+        }
+        if (node.point.equals(key)) {
+            return node;
+        }
+        double comp = 0;
+        if (xy) {
+            comp = node.point.getX() - key.getX();
+        } else if (!xy) {
+            comp = node.point.getY() - key.getY();
+        }
+
+        if (comp > 0) {
+            node.left = insert(node.left, key, !xy);
+        } else {
+            node.right = insert(node.right, key, !xy);
+        }
+        return node;
+
     }
 
-    */
 
     /* Returns the closest Point to the inputted X and Y coordinates. This method
        should run in O(log N) time on average, where N is the number of POINTS. */
     public Point nearest(double x, double y) {
-        // TODO: YOUR CODE HERE
-        return null;
+        return nearestHelper(new Point(x, y), root, root.point);
     }
+
+    public Point nearestHelper(Point g, KDTreeNode node, Point best) {
+        if (node == null) {
+            return best;
+        }
+        double dist = Point.distance(g, node.point);
+        KDTreeNode good;
+        KDTreeNode bad;
+        boolean useful = false;
+
+        if (dist < Point.distance(g, best)) {
+            best = node.point;
+        }
+
+        int xy = find(root, node.point, 1);
+        if (xy == 1) {
+            if (g.getX() < node.point.getX()) {
+                good = node.left;
+                bad = node.right;
+            } else {
+                good = node.right;
+                bad = node.left;
+            }
+        } else {
+            if (g.getY() < node.point.getY()) {
+                good = node.left;
+                bad = node.right;
+            } else {
+                good = node.right;
+                bad = node.left;
+            }
+        }
+        best = nearestHelper(g, good, best);
+
+
+        if (xy == 1) {
+            if (bad != null && Math.abs(g.getY() - bad.point.getY()) < Point.distance(g, best)) {
+                best = nearestHelper(g, bad, best);
+            }
+        } else {
+            if (bad != null && Math.abs(g.getX() - bad.point.getX()) < Point.distance(g, best)) {
+                best = nearestHelper(g, bad, best);
+            }
+        }
+
+        return best;
+
+    }
+
+
+    public int find(KDTreeNode node, Point target, int row) {
+        if (node.point == target) {
+            return row % 2;
+        }
+
+        if (row % 2 == 1) {
+            if (target.getX() < node.point.getX()) {
+                return find(node.left, target, row + 1);
+            } else {
+                return find(node.right, target, row + 1);
+            }
+        } else {
+            if (target.getY() < node.point.getY()) {
+                return find(node.left, target, row + 1);
+            } else {
+                return find(node.right, target, row + 1);
+            }
+        }
+    }
+
 
     private class KDTreeNode {
 
--- lab18/KDTreeTest.java
+++ lab18/KDTreeTest.java
@@ -1,9 +1,11 @@
 import java.util.List;
 import java.util.LinkedList;
 import java.util.Random;
+
 import org.junit.Test;
 
 import static org.junit.Assert.*;
+
 /* Test suite for the KDTree.
    @author Christine Zhou */
 public class KDTreeTest {
@@ -52,15 +54,17 @@ public class KDTreeTest {
         for (Point p : queryPoints) {
             naiveNearest.add(naive.nearest(p.getX(), p.getY()));
         }
-        System.out.println(String.format("The NaivePointSet took %s seconds to perform 200 queries.", sw0.elapsedTime()));
+        System.out.println(String.format("The NaivePointSet took %s seconds to " +
+                "perform 200 queries.", sw0.elapsedTime()));
 
-        Stopwatch sw1= new Stopwatch();
+        Stopwatch sw1 = new Stopwatch();
         for (int i = 0; i < 200; i++) {
             double lon = queryPoints.get(i).getX();
             double lat = queryPoints.get(i).getY();
             assertEquals(naiveNearest.get(i), kd.nearest(lon, lat));
         }
-        System.out.println(String.format("The KDTree took %s seconds to perform 200 queries.", sw1.elapsedTime()));
+        System.out.println(String.format("The KDTree took %s seconds to " +
+                "perform 200 queries.", sw1.elapsedTime()));
     }
 
     /* Creates 10,000 random points and 2,000 query points. Insert the 10,000
@@ -78,15 +82,17 @@ public class KDTreeTest {
         for (Point p : queryPoints) {
             naiveNearest.add(naive.nearest(p.getX(), p.getY()));
         }
-        System.out.println(String.format("The NaivePointSet took %s seconds to perform 2,000 queries.", sw0.elapsedTime()));
+        System.out.println(String.format("The NaivePointSet took %s seconds to " +
+                "perform 2,000 queries.", sw0.elapsedTime()));
 
-        Stopwatch sw1= new Stopwatch();
+        Stopwatch sw1 = new Stopwatch();
         for (int i = 0; i < 2000; i++) {
             double lon = queryPoints.get(i).getX();
             double lat = queryPoints.get(i).getY();
             assertEquals(naiveNearest.get(i), kd.nearest(lon, lat));
         }
-        System.out.println(String.format("The KDTree took %s seconds to perform 2,000 queries.", sw1.elapsedTime()));
+        System.out.println(String.format("The KDTree took %s seconds to " +
+                "perform 2,000 queries.", sw1.elapsedTime()));
     }
 
     // Your tests here!
